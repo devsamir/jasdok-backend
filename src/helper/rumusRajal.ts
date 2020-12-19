7371a3e59b1fdfa852a4bffec7b14787521b4967
devsamir <samir.batog@gmail.com>
2020-12-19 12:33:45 +0700

    Fix Rumus Rajal

--- src/helper/rumusRajal.ts
+++ src/helper/rumusRajal.ts
@@ -23,15 +23,16 @@ const special = [
 const hitungSri = (rajal: any, sri: any) => {
   const tarifNormal = normal.reduce((acc: any, curr: string) => {
     const tarif_rs = rajal[curr] - sri[curr];
+    const totalTarifRs = rajal.tarifRs - sri.tarifRs;
     acc[curr] = {
       rajal: {
         tarif_rs,
-        persen: Math.round((tarif_rs / rajal.tarifRs) * 10000) / 100,
+        persen: Math.round((tarif_rs / totalTarifRs) * 10000) / 100,
         tarif_ina: Math.round((rajal.tarifIna * rajal[curr]) / rajal.tarifRs),
       },
       sri: {
         tarif_rs: sri[curr],
-        persen: Math.round((sri[curr] / rajal.tarifRs) * 10000) / 100,
+        persen: Math.round((sri[curr] / sri.tarifRs) * 10000) / 100,
         tarif_ina: Math.round((rajal.tarifIna * sri[curr]) / sri.tarifRs),
       },
     };
@@ -39,6 +40,7 @@ const hitungSri = (rajal: any, sri: any) => {
   }, {});
   const tarifSpecial = special.reduce((acc: any, curr: string) => {
     const tarif_rs = rajal[curr] - sri[curr];
+    const totalTarifRs = rajal.tarifRs - sri.tarifRs;
     const tarif_sarana = Math.round((tarif_rs * 30) / 100);
     const tarif_pelayanan = Math.round((tarif_rs * 70) / 100);
     const sri_sarana = Math.round((sri[curr] * 30) / 100);
@@ -47,14 +49,14 @@ const hitungSri = (rajal: any, sri: any) => {
       rajal: {
         sarana: {
           tarif_rs: tarif_sarana,
-          persen: Math.round((tarif_sarana / rajal.tarifRs) * 10000) / 100,
+          persen: Math.round((tarif_sarana / totalTarifRs) * 10000) / 100,
           tarif_ina: Math.round(
             (rajal.tarifIna * tarif_sarana) / rajal.tarifRs
           ),
         },
         pelayanan: {
           tarif_rs: tarif_pelayanan,
-          persen: Math.round((tarif_pelayanan / rajal.tarifRs) * 10000) / 100,
+          persen: Math.round((tarif_pelayanan / totalTarifRs) * 10000) / 100,
           tarif_ina: Math.round(
             (rajal.tarifIna * tarif_pelayanan) / rajal.tarifRs
           ),
@@ -63,12 +65,12 @@ const hitungSri = (rajal: any, sri: any) => {
       sri: {
         sarana: {
           tarif_rs: sri_sarana,
-          persen: Math.round((sri_sarana / rajal.tarifRs) * 10000) / 100,
+          persen: Math.round((sri_sarana / sri.tarifRs) * 10000) / 100,
           tarif_ina: Math.round((rajal.tarifIna * sri_sarana) / rajal.tarifRs),
         },
         pelayanan: {
           tarif_rs: sri_pelayanan,
-          persen: Math.round((sri_pelayanan / rajal.tarifRs) * 10000) / 100,
+          persen: Math.round((sri_pelayanan / sri.tarifRs) * 10000) / 100,
           tarif_ina: Math.round(
             (rajal.tarifIna * sri_pelayanan) / rajal.tarifRs
           ),
